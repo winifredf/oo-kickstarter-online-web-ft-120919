7a231cf1ed82dd342b23ed6c563432d92a5269ad
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-12 16:58:36 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+class Project
+  
+  attr_accessor :title, :backers
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def back_project(project)
+    @back_project = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_project(self) unless backer.backed_projects.include?(self)
+  end
+end
