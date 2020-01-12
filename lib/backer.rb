93d370cb83eee592ef271cd118b19fe85b74192c
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-12 17:49:52 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,22 +1,3 @@
-class Backer
-  
-  attr_reader :name, :backed_projects
-  
-  def initialize(name)
-    @name = name
-    @backed_projects = []
-  end
-  
-  def back_project
-    pbrs = ProjectBacker.all select {|pbr| pbr.backer == self}
-    pbrs.map {|pbr| prb.project}
-  end
-  
-  def back_project(project)
-    ProjectBacker.new(project, self)
-  end
-end
-
 class ProjectBacker
   attr_reader :project, :backer
   
